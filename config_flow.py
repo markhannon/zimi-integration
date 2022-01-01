61e5c6ab21369665922c21ed6c045c0c45b63838
Mark Hannon <mark.hannon@gmail.com>
2022-01-01 02:09:31 +0000

    Initial zimi component

---
+++ config_flow.py
@@ -0,0 +1,118 @@
+"""Config flow for zcc integration."""
+from __future__ import annotations
+
+import logging
+import pprint
+from typing import Any
+
+import voluptuous as vol
+from zcc import ControlPoint, ControlPointError
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_HOST, CONF_PORT
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResult
+from homeassistant.exceptions import HomeAssistantError
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+# TODO adjust the data schema to the data that you need
+STEP_USER_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_HOST): str,
+        vol.Required(CONF_PORT): int,
+    }
+)
+
+
+class ZimiHub:
+    """Zimi HUB class."""
+
+    def __init__(self, host: str, port: int) -> None:
+        """Initialize."""
+        self.host = host
+        self.port = port
+        _LOGGER.info("ControlPoint inititation starting")
+        try:
+            self.zcc = ControlPoint(host=self.host, port=self.port)
+        except ControlPointError as e:
+            _LOGGER.info("ControlPoint initiation failed:" + e)
+        _LOGGER.info("ControlPoint inititation completed")
+        _LOGGER.info(self.zcc.describe())
+
+    async def authenticate(self, username: str, password: str) -> bool:
+        """Test if we can authenticate with the host."""
+        return True
+
+
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+    """Validate the user input allows us to connect.
+
+    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
+    """
+    # TODO validate the data can be used to set up a connection.
+
+    # If your PyPI package is not built with async, pass your methods
+    # to the executor:
+    # await hass.async_add_executor_job(
+    #     your_validate_func, data["username"], data["password"]
+    # )
+
+    _LOGGER.info(pprint.pformat(hass))
+    _LOGGER.info(pprint.pformat(data))
+
+    hub = ZimiHub(data["host"], data["port"])
+
+    if not await hub.authenticate(data["host"], data["port"]):
+        raise InvalidAuth
+
+    # If you cannot connect:
+    # throw CannotConnect
+    # If the authentication is wrong:
+    # InvalidAuth
+
+    # Return info that you want to store in the config entry.
+    return {"title": "Name of the device"}
+
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for zcc."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the initial step."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+            )
+
+        errors = {}
+
+        try:
+            info = await validate_input(self.hass, user_input)
+        except CannotConnect:
+            errors["base"] = "cannot_connect"
+        except InvalidAuth:
+            errors["base"] = "invalid_auth"
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        else:
+            return self.async_create_entry(title=info["title"], data=user_input)
+
+        return self.async_show_form(
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+        )
+
+
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(HomeAssistantError):
+    """Error to indicate there is invalid auth."""
