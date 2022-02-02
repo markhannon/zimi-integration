14f0e4c0cd870a1a019c2b8d5fbce3ea8195aff6
Mark Hannon <mark.hannon@gmail.com>
2022-02-02 05:06:36 +0000

    Remove abilty to reload

--- __init__.py
+++ __init__.py
@@ -18,8 +18,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Connect to Zimi Controller and register device."""
 
     _LOGGER.info("Starting async_setup_entry")
-    _LOGGER.debug("entry_id: %s", entry.entry_id)
-    _LOGGER.debug("data:     %s", pprint.pformat(entry.data))
+    _LOGGER.info("entry_id: %s", entry.entry_id)
+    _LOGGER.info("data:     %s", pprint.pformat(entry.data))
+
     controller = ZimiController(hass, entry)
     connected = controller.connect()
     if not connected:
@@ -38,12 +39,3 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
 
     return True
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
