72fc684c4a65696e812612afc8fbe8d2bb2a707e
Mark Hannon <mark.hannon@gmail.com>
2024-10-28 12:14:05 +1100

    Proposed merge strategy

--- merge.sh
+++ merge.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
+# Parallel Development Workflow
+#
+# 1.Upstream development is done in a seperately checked out repo $UPSTREAM_REPO and $UPSTREAM_BRANCH
+# 2.Downstream merge is done in this repo by running this script to copy latest  
 
-UPSTREAM_REPO=/home/mark/Projects/core-integration-zimi
-UPSTREAM_BRANCH=zimi-integration
+MERGE_BRANCH=upstream
+UPSTREAM_REPO=../core-zimi
+UPSTREAM_BRANCH=zimi
 
 echo Updating upstream repo
 (cd ${UPSTREAM_REPO}; git pull origin ${UPSTREAM_BRANCH}; git checkout ${UPSTREAM_BRANCH})
 
-echo Importing zimi contents
+
+echo Importing zimi contents into $MERGE_BRANCH branch
+git checkout -b $MERGE_BRANCH
 cp ${UPSTREAM_REPO}/homeassistant/components/zimi/*.py custom_components/zimi
+git add custome_components/zimi/*
+git commit -m 'Merge of latest upstream'
 
 echo Checking git status
 git status
 
+echo Perform manual merge from $MERGE_BRANCH as required
