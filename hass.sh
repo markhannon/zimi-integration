abe04577e0f491d4b252af605907b4557a4e4cab
Mark Hannon <mark.hannon@gmail.com>
2024-11-14 11:35:29 +1100

    Update hass.sh to work under WSL

--- hass.sh
+++ hass.sh
@@ -3,8 +3,16 @@
 # Setup and optionally run local HA server in virtual environment
 # 
 
+
+PYTHON_HOMEBREW=/opt/homebrew/bin/python3
+if [ -e "$PYTHON_HOMEBREW" ]; then
+    export PYTHON_BOOTSTRAP=$PYTHON_HOMEBREW
+else
+    export PYTHON_BOOTSTRAP=/usr/bin/python3
+fi
+
 echo "Creating virtual environment for HA"
-/opt/homebrew/bin/python3 -m venv .venv
+$PYTHON_BOOTSTRAP -m venv .venv
 .venv/bin/python3 -m pip install wheel
 .venv/bin/python3 -m pip install homeassistant
 
@@ -12,7 +20,7 @@ echo "Creating config and symlinking custom_components"
 .venv/bin/hass --script ensure_config -c .venv/config
 (cd .venv/config; ln -s ../../custom_components)
 
-if [ "$1" == "--execute" ]; then
+if [ "$1" = "--execute" ]; then
     echo "Starting HA on localhost:8123"
     .venv/bin/hass -c .venv/config
 fi
