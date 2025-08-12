0001e3e5663b5d26ea74b137c9e67164710cba46
Mark Hannon <mark.hannon@gmail.com>
2025-08-12 11:01:03 +1000

    Set default position in async_set_cover_position()

--- custom_components/zimi/cover.py
+++ custom_components/zimi/cover.py
@@ -85,10 +85,12 @@ class ZimiCover(ZimiEntity, CoverEntity):
 
     async def async_set_cover_position(self, **kwargs: Any) -> None:
         """Open the cover/door to a specified percentage."""
-        if position := kwargs.get("position"):
-            _LOGGER.debug("Sending set_cover_position(%d) for %s",
-                          position, self.name)
-            await self._device.open_to_percentage(position)
+        position = kwargs.get("position")
+        if position is None:
+            position = 0
+        _LOGGER.debug("Sending set_cover_position(%d) for %s",
+                      position, self.name)
+        await self._device.open_to_percentage(position)
 
     async def async_stop_cover(self, **kwargs: Any) -> None:
         """Stop the cover."""
